fe41c172b72e96c14dffed1c7200b23382ed2742
RVRX <colemanning@yandex.com>
2020-08-18 23:00:13 -0400

    added scrollify smooth-scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Remove navbar on scroll
+
 //https://stackoverflow.com/a/54164631
 // ^ Credit
 
@@ -14,6 +16,30 @@
         } else {
             $('.navbar').removeClass("navbar-hide");
         }
+
+        //
+        if (scroll >= 30) {
+            // $('.name').removeClass("animate__animated animate__fadeOutUp");
+            $('.hero-bottom').addClass("animate__animated animate__fadeOutDown");
+        } else {
+            $('.hero-bottom').removeClass("animate__animated animate__fadeOutDown");
+        }
+
+        // if (scroll >= 10 && scroll < 450) {
+        //     document.getElementById("about-anchorpoint").scrollIntoView();
+        //     // $('#projects').scrollIntoView();
+        //     console.log("Scrolled Into View")
+        //     // console.log(document.getElementById("projects").scrollHeight);
+        //     // console.log(document.getElementById("projects").scrollTop);
+        // }
+        //
+        // if (scroll >= 900 && scroll < 2000) {
+        //     document.getElementById("name").scrollIntoView();
+        //     // $('#projects').scrollIntoView();
+        //     console.log("Scrolled Into View22")
+        //     // console.log(document.getElementById("projects").scrollHeight);
+        //     // console.log(document.getElementById("projects").scrollTop);
+        // }
 
     });
 
